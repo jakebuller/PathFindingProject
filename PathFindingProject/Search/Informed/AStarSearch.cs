b3cc21c9881498b1d4d05b7ee9b5a71300e1326e
Michael Dougall <michaelrsdougall@gmail.com>
2013-03-11 22:13:23 -0400

    more ports

---
+++ PathFindingProject/Search/Informed/AStarSearch.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using PathFindingProject.Search.Framework;
+
+namespace PathFindingProject.Search.Informed
+{
+
+    /**
+	 * Constructs an A* search from the specified search problem and heuristic
+	 * function
+	 * 
+	 * @param search
+	 *            a search problem
+	 * @param hf
+	 *            a heuristic function <em>h(n)</em>, which estimates the cost
+	 *            of the cheapest path from the state at node <em>n</em> to a
+	 *            goal state.
+	 */
+
+    public class AStarSearch: BestFirstSearch {
+        public AStarSearch(QueueSearch search, HeuristicFunction hf) : base(search, new AStarSearchEvaluationFunction(hf)) {}
+    }
+}
---
+++ PathFindingProject/Search/Informed/AStarSearchEvaluationFunction.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using PathFindingProject.Search.Framework;
+
+namespace PathFindingProject.Search.Informed
+{
+    public class AStarSearchEvaluationFunction
+    {
+        private PathCostFunction gf = new PathCostFunction();
+        private HeuristicFunction hf = null;
+
+        public AStarSearchEvaluationFunction(HeuristicFunction hf) {
+		    this.hf = hf;
+	    }
+
+        public double f(Node n){
+            return gf.g(n) + hf.h(n.State);
+        }
+
+
+    }
+}
