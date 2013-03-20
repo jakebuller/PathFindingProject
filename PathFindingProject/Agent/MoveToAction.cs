10a060813d96507b6809947da1f63627a0245b30
Travis Miehm <travis.miehm@gmail.com>
2013-03-20 17:30:09 -0400

    Forgot a file

---
+++ PathFindingProject/Agent/MoveToAction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PathFindingProject.Agent {
+	public class MoveToAction : DynamicAction {
+		public static string MoveToLocationAttribute = "location";
+
+		public MoveToAction( string location ) : base( "moveTo" ) {
+			SetAttribute( MoveToLocationAttribute, location );
+		}
+
+		public string GetToLocation() {
+			return ( string )GetAttribute( MoveToLocationAttribute );
+		}
+	}
+}
