630e5ab796e8db5e750c084c45db2ecad4b97485
Travis Miehm <travis.miehm@gmail.com>
2013-03-26 18:14:48 -0400

    Improving text file parsing and problem setup

--- PathFindingProject/Program.cs
+++ PathFindingProject/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Text;
 using System.Collections.Generic;
 using System.Linq;
 
 using PathFindingProject.Search.Domain;
+using PathFindingProject.Search.Framework;
 using PathFindingProject.Environment.Map;
 
 namespace PathFindingProject {
@@ -10,87 +12,45 @@ namespace PathFindingProject {
 
         private static int DimX;
         private static int DimY;
-        private static List<Point> Robots;
+        private static List<Point> Robots = new List<Point>();
         private static Point Rendevous;
-        private static ExtendableMap ProblemMap;
+        private static ExtendableMap ProblemMap = new ExtendableMap();
+		private static double Distance = 1;
 
         public static int Main( string[] args ) {
             string[] lines = System.IO.File.ReadAllLines( @"../../Maps/map_1.txt" );
-            string[] coords;
             if( lines.Length < 6 ) {
                 //Insufficient parameters
                 return -1;
             }
-            //get the room dimensions
-            string[] dims = lines[0].Split( ' ' );
-            DimY = int.Parse( dims[1] );
-            DimX = int.Parse( dims[0] );
+			// first line has the x and then y size
+			SetRoomDimensions( lines[0] );
 
-            Robots = new List<Point>();
-            //get the robots
-            int NumRobot = int.Parse( lines[1] );
-            for( int i = 0; i < NumRobot; i++ ) {
-                coords = lines[i + 2].Split( ' ' );
-                int x = int.Parse( coords[0] );
-                int y = int.Parse( coords[1] );
-                Point NextPoint = new Point( x, y );
-                Robots.Add( NextPoint );
-            }
+            SetRobots(lines);
 
-            coords = lines[Robots.Count + 2].Split( ' ' );
-            Rendevous = new Point( int.Parse( coords[0] ), int.Parse( coords[1] ) );
+			// 2 + Robots.Count lines appear before the rendevous point
+			SetRendevousPoint( lines[ 2 + Robots.Count] );
 
+			// where the floor plan starts in the text file
             int depth = 3 + Robots.Count();
 
-            //Map = new int[DimX,DimY];
-            ProblemMap = new ExtendableMap();
-            int[,] Map = new int[DimX, DimY];
             for( int i = 0; i < DimX; i++ ) {
                 char[] line = lines[i + depth].ToCharArray();
+
                 for( int j = 0; j < line.Length; j++ ) {
-                    Map[i, j] = ( int )Char.GetNumericValue( line[j] );
-                    Console.Write( Map[i, j] );
+					int pointValue = ( int )Char.GetNumericValue( line[j] );
+					Console.Write( pointValue );
+
+					if( pointValue == 1 ) {
+						continue;
+					}
+					var label = i + "," + j;
+					ProblemMap.SetPosition( label, i, j );
+
+					AddLinks( i, j, label );
                 }
                 Console.WriteLine();
             }
-
-
-            for( int i = 0; i < DimX; i++ ) {
-                for( int j = 0; j < DimY; j++ ) {
-                    if( Map[i, j] == 0 ) {
-                        Console.WriteLine( "Adding point for " + i + "," + j );
-                        //Check the boundaries
-                        //Then check the neighbour before adding the link
-                        //if the neighbour is 1 don't add the link
-                        if( j != 0 ) {
-                            if( Map[i, j - 1] == 0 ) {
-                                ProblemMap.AddBidirectionalLink( i + "," + j, i + "," + ( j - 1 ), 1 );
-                                Console.WriteLine( "Added link to: " + i + "," + ( j - 1 ) );
-                            }
-                        }
-                       if( j < DimY - 1 ) {
-                            if( Map[i, j + 1] == 0 ) {
-                                ProblemMap.AddBidirectionalLink( i + "," + j, i + "," + ( j + 1 ), 1 );
-                                Console.WriteLine( "Added link to: " + i + "," + ( j + 1 ) );
-                            }
-                        }
-                        if( i != 0 ) {
-                            if( Map[i - 1, j] == 0 ) {
-                                ProblemMap.AddBidirectionalLink( i + "," + j, ( i - 1 ) + "," + j, 1 );
-                                Console.WriteLine( "Added link to: " + ( i - 1 ) + "," + j );
-                            }
-                        }
-                        if( i < DimX - 1) {
-                            if( Map[i + 1, j] == 0 ) {
-                                ProblemMap.AddBidirectionalLink( i + "," + j, ( i + 1 ) + "," + j, 1 );
-                                Console.WriteLine( "Added link to: " + ( i + 1 ) + "," + j );
-                            }
-                        }
-                    }
-                }
-                Console.WriteLine();
-            }
-
 
             Console.WriteLine( "Rendevous at x: " + Rendevous.XCoord + " y: " + Rendevous.YCoord );
             Console.WriteLine( "Robots: " + Robots.Count );
@@ -101,6 +61,69 @@ namespace PathFindingProject {
 #endif
             return 0;
         }
+
+		private static void AddLinks( int x, int y, string currentLabel ) {
+			var topNeighbourLabel = x + "," + ( y + 1 );
+			if( ProblemMap.IsLocation( topNeighbourLabel ) ) {
+				ProblemMap.AddBidirectionalLink( 
+					currentLabel, 
+					topNeighbourLabel,
+					Distance
+				);
+			}
+
+			var leftNeighbourLabel = ( x - 1 ) + "," + y;
+			if( ProblemMap.IsLocation( leftNeighbourLabel ) ) {
+				ProblemMap.AddBidirectionalLink(
+					currentLabel,
+					leftNeighbourLabel,
+					Distance
+				);
+			}
+
+			var rightNeighbourLabel = ( x + 1 ) + "," + y;
+			if( ProblemMap.IsLocation( rightNeighbourLabel ) ) {
+				ProblemMap.AddBidirectionalLink(
+					currentLabel,
+					rightNeighbourLabel,
+					Distance
+				);
+			}
+
+			var bottomNeighbourLabel = x + "," + ( y - 1 );
+			if( ProblemMap.IsLocation( bottomNeighbourLabel ) ) {
+				ProblemMap.AddBidirectionalLink(
+					currentLabel,
+					bottomNeighbourLabel,
+					Distance
+				);
+			}
+		}
+
+		private static void SetRendevousPoint( string line ) {
+			string[] coords = line.Split( ' ' );
+			Rendevous = new Point( int.Parse( coords[0] ), int.Parse( coords[1] ) );
+		}
+
+		private static void SetRobots(string[] lines) {
+			string[] coords;
+			// second line has the number of robots
+			int numRobots = int.Parse( lines[1] );
+			for( int i = 0; i < numRobots; i++ ) {
+				// + 2 to always skip the first two lines of the file
+				coords = lines[i + 2].Split( ' ' );
+				int x = int.Parse( coords[0] );
+				int y = int.Parse( coords[1] );
+				Point point = new Point( x, y );
+				Robots.Add( point );
+			}
+		}
+
+		private static void SetRoomDimensions( string line ) {
+			string[] dims = line.Split( ' ' );
+			DimX = int.Parse( dims[0] );
+			DimY = int.Parse( dims[1] );
+		}
     }
 
 }
